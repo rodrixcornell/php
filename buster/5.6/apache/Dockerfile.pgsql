ed6a37f61c73dfae1c61b1d13feb5a8a72b5f64b
Rodrigo Cabral <rodrigo.cabral@pmm.am.gov.br>
2021-06-17 15:49:50 -0400

    No ramo master
    * Mudanças a serem submetidas:
    	modified:   buster/5.6/apache/Dockerfile
    	modified:   buster/5.6/apache/Dockerfile.mysql
    	modified:   buster/5.6/apache/Dockerfile.oracle
    	modified:   buster/5.6/apache/Dockerfile.pgsql
    	modified:   buster/5.6/apache/buildDocker.sh
    	modified:   buster/5.6/cli/Dockerfile
    	modified:   buster/5.6/cli/Dockerfile.mysql
    	modified:   buster/5.6/cli/Dockerfile.oracle
    	modified:   buster/5.6/cli/Dockerfile.pgsql
    	modified:   buster/5.6/cli/buildDocker.sh
    	modified:   buster/5.6/fpm/Dockerfile
    	modified:   buster/5.6/fpm/Dockerfile.mysql
    	modified:   buster/5.6/fpm/Dockerfile.oracle
    	modified:   buster/5.6/fpm/Dockerfile.pgsql
    	modified:   buster/5.6/fpm/buildDocker.sh
    
    @rodrixcornell Rodrigo Cabral <rodrixcornell@gmail.com>

--- buster/5.6/apache/Dockerfile.pgsql
+++ buster/5.6/apache/Dockerfile.pgsql
@@ -9,32 +9,37 @@ FROM php:${php_version:+${php_version}-}${variant:+${variant}-}${codename:+${cod
 
 MAINTAINER Rodrigo Cabral <rodrixcornell@gmail.com.br>
 
-ENV TIMEZONE="America/Manaus" \
-	LC_ALL="C.UTF-8" \
-	LANG="pt_BR.utf8" \
-	LANGUAGE="pt_BR:pt"
+ENV TZ="America/Manaus" \
+LC_ALL="C.UTF-8" \
+LANG="pt_BR.utf8" \
+LANGUAGE="pt_BR:pt"
 
 RUN set -xe \
-	&& echo $TIMEZONE | tee /etc/timezone \
-	&& apt-get update -y && apt-get upgrade -yq --no-install-recommends \
-	&& apt-get install -y --no-install-recommends locales software-properties-common \
-	&& localedef -i pt_BR -c -f UTF-8 -A /usr/share/locale/locale.alias pt_BR.UTF-8 \
-	&& locale-gen pt_BR.UTF-8 \
-	&& apt-get install -y --no-install-recommends \
-		libfreetype6-dev \
-		libjpeg62-turbo-dev \
-		libpng-dev \
-		libpq-dev \
-	&& rm -rf /var/lib/apt/lists/* \
-	\
-	&& docker-php-source extract \
-	&& docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/ \
-	&& docker-php-ext-configure pgsql --with-pgsql=/usr/include/ \
-	&& docker-php-ext-install -j$(nproc) gd bcmath exif pgsql pdo_pgsql \
-	&& docker-php-ext-enable gd bcmath exif pgsql pdo_pgsql \
-	&& docker-php-source delete \
-	&& apt-get purge -y locales software-properties-common && apt-get autoclean && apt-get autoremove -y && rm -rf /var/lib/apt/lists/* && rm -rf /usr/lib/python3 \
-	&& apt-get update -y && apt-get upgrade -yq --no-install-recommends && apt-get autoclean && apt-get autoremove -y && rm -rf /var/lib/apt/lists/*
+&& a2enmod rewrite headers expires \
+&& apt-get update -y && apt-get upgrade -yq --no-install-recommends \
+&& apt-get install -y --no-install-recommends git locales software-properties-common \
+&& echo $TZ | tee /etc/timezone \
+&& cp -rf /usr/share/zoneinfo/$TZ /etc/localtime \
+&& localedef -i pt_BR -c -f UTF-8 -A /usr/share/locale/locale.alias pt_BR.UTF-8 \
+&& locale-gen pt_BR.UTF-8 \
+&& apt-get install -y --no-install-recommends \
+libfreetype6-dev \
+libjpeg-dev \
+libpng-dev \
+libicu-dev \
+libpq-dev \
+&& rm -rf /var/lib/apt/lists/* \
+\
+&& docker-php-source extract \
+&& docker-php-ext-configure intl \
+&& docker-php-ext-configure gd --with-freetype --with-jpeg \
+&& docker-php-ext-configure pgsql --with-pgsql=/usr/include/ \
+&& docker-php-ext-configure pdo_pgsql --with-pdo-pgsql=/usr/include/ \
+&& docker-php-ext-install -j$(nproc) bcmath exif intl gd pgsql pdo_pgsql \
+&& docker-php-ext-enable bcmath exif intl gd pgsql pdo_pgsql \
+&& docker-php-source delete \
+&& apt-get purge -y locales software-properties-common && apt-get autoclean && apt-get autoremove -y && rm -rf /var/lib/apt/lists/* && rm -rf /usr/lib/python3 \
+&& apt-get update -y && apt-get upgrade -yq --no-install-recommends && apt-get autoclean && apt-get autoremove -y && rm -rf /var/lib/apt/lists/*
 
 # Use the default production configuration
 # RUN mv "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/php.ini"
--- buster/5.6/apache/buildDocker.sh
+++ buster/5.6/apache/buildDocker.sh
@@ -5,6 +5,10 @@ echo -e "buildDocker.sh\r";
 
 export $(egrep -v '^#' .env | xargs)
 
+IMAGE_NAME="rodrixcornell/php:$(echo $php_version | cut -c1-3)"
+
+IMAGE_BUILD="--build-arg php_version=${php_version} --build-arg variant=${variant} --build-arg codename=${codename} --build-arg release=${release} --build-arg distribution=${distribution}"
+
 # Proxy settings
 PROXY_SETTINGS=""
 if [ "${http_proxy}" != "" ]; then
@@ -27,54 +31,50 @@ if [ "$PROXY_SETTINGS" != "" ]; then
   echo "Proxy settings were found and will be used during the build."
 fi
 
-IMAGE_NAME="rodrixcornell/php:$(echo $php_version | cut -c1-3)"
-
-IMAGE_BUILD="--build-arg php_version=${php_version} --build-arg variant=${variant} --build-arg codename=${codename} --build-arg release=${release} --build-arg distribution=${distribution}"
-
 docker build --force-rm --no-cache $IMAGE_BUILD $PROXY_SETTINGS \
---tag $IMAGE_NAME-${variant}-buster \
+--tag $IMAGE_NAME-${variant}-${codename} \
 --tag $IMAGE_NAME-${variant} \
---tag rodrixcornell/php:${variant}-buster \
+--tag rodrixcornell/php:${variant}-${codename} \
 --tag rodrixcornell/php:${variant} \
 --file Dockerfile .
 
 docker build --force-rm --no-cache $IMAGE_BUILD $PROXY_SETTINGS \
---tag $IMAGE_NAME-mysql-${variant}-buster \
+--tag $IMAGE_NAME-mysql-${variant}-${codename} \
 --tag $IMAGE_NAME-mysql-${variant} \
---tag rodrixcornell/php:mysql-${variant}-buster \
+--tag rodrixcornell/php:mysql-${variant}-${codename} \
 --tag rodrixcornell/php:mysql-${variant} \
 --file Dockerfile.mysql .
 
 docker build --force-rm --no-cache $IMAGE_BUILD $PROXY_SETTINGS \
---tag $IMAGE_NAME-oci-${variant}-buster \
---tag $IMAGE_NAME-oci-${variant} \
---tag rodrixcornell/php:oci-${variant}-buster \
---tag rodrixcornell/php:oci-${variant} \
---file Dockerfile.oracle .
-
-docker build --force-rm --no-cache $IMAGE_BUILD $PROXY_SETTINGS \
---tag $IMAGE_NAME-pgsql-${variant}-buster \
+--tag $IMAGE_NAME-pgsql-${variant}-${codename} \
 --tag $IMAGE_NAME-pgsql-${variant} \
---tag rodrixcornell/php:pgsql-${variant}-buster \
+--tag rodrixcornell/php:pgsql-${variant}-${codename} \
 --tag rodrixcornell/php:pgsql-${variant} \
 --file Dockerfile.pgsql .
 
-docker push $IMAGE_NAME-${variant}-buster && \
+docker build --force-rm --no-cache $IMAGE_BUILD $PROXY_SETTINGS \
+--tag $IMAGE_NAME-oci-${variant}-${codename} \
+--tag $IMAGE_NAME-oci-${variant} \
+--tag rodrixcornell/php:oci-${variant}-${codename} \
+--tag rodrixcornell/php:oci-${variant} \
+--file Dockerfile.oracle .
+
+docker push $IMAGE_NAME-${variant}-${codename} && \
 docker push $IMAGE_NAME-${variant} && \
-docker push rodrixcornell/php:${variant}-buster && \
+docker push rodrixcornell/php:${variant}-${codename} && \
 docker push rodrixcornell/php:${variant}
 
-docker push $IMAGE_NAME-mysql-${variant}-buster && \
+docker push $IMAGE_NAME-mysql-${variant}-${codename} && \
 docker push $IMAGE_NAME-mysql-${variant}
-docker push rodrixcornell/php:mysql-${variant}-buster && \
+docker push rodrixcornell/php:mysql-${variant}-${codename} && \
 docker push rodrixcornell/php:mysql-${variant}
 
-docker push $IMAGE_NAME-oci-${variant}-buster && \
-docker push $IMAGE_NAME-oci-${variant}
-docker push rodrixcornell/php:oci-${variant}-buster && \
-docker push rodrixcornell/php:oci-${variant}
-
-docker push $IMAGE_NAME-pgsql-${variant}-buster && \
+docker push $IMAGE_NAME-pgsql-${variant}-${codename} && \
 docker push $IMAGE_NAME-pgsql-${variant}
-docker push rodrixcornell/php:pgsql-${variant}-buster && \
+docker push rodrixcornell/php:pgsql-${variant}-${codename} && \
 docker push rodrixcornell/php:pgsql-${variant}
+
+docker push $IMAGE_NAME-oci-${variant}-${codename} && \
+docker push $IMAGE_NAME-oci-${variant}
+docker push rodrixcornell/php:oci-${variant}-${codename} && \
+docker push rodrixcornell/php:oci-${variant}
